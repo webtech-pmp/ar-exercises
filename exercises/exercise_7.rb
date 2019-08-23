137fa1a2be2c9452a0c834bce607360d86218388
Nancy <webtech.pmp@gmail.com>
2019-08-22 19:51:18 -0700

    add Exercise 7 code

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,29 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+Employee.all.each do |e_info|
+  if e_info.first_name && e_info.last_name && e_info.hourly_rate > 40 && e_info.hourly_rate < 200 && e_info.store_id
+    puts "Employee id: #{e_info.id} has all the required information."
+  else
+    puts "Employee id: #{e_info.id} does not have the required information, please re-enter."
+  end
+end
+
+Store.all.each do |s_info|
+  if s_info.name.length >= 3 && s_info.annual_revenue.to_i >= 0 && (s_info.mens_apparel || s_info.womens_apparel)
+    puts "Store id: #{s_info.id} has all the required information."
+  else
+    puts "Store id: #{s_info.id} does not have the required information, please re-enter."
+  end
+end
+
+puts "Entering new store information >"
+puts "Please enter a store name:"
+name = $stdin.gets.chomp
+puts "Please enter the store annual revenue:"
+annual_revenue = $stdin.gets.chomp
+puts "Does this store carry men's apparel? (true or false):"
+mens_apparel = $stdin.gets.chomp
+puts "Does this store carry women's apparel? (true or false):"
+womens_apparel = $stdin.gets.chomp
